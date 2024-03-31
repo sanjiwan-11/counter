87b2815d524b045bceded89f0b88fec6e895b6d8
Sanjiwan Mahat <sanjiwanmahat2002@gmail.com>
2024-03-31 22:02:48 +0530

    JS file for Counter

---
+++ app.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector(".increase");
+const val = document.querySelector("#value");
+const btn2 = document.querySelector(".decrease");
+const btn1 = document.querySelector(".reset");
+
+btn.addEventListener("click", () => {
+    val.innerText++;
+    if(val.innerText>0){
+        val.style.color = "green";
+    }else if(val.innerText == 0){
+        val.style.color = "black";
+    }
+});
+
+btn1.addEventListener("click",()=>{
+    val.innerHTML = 0;
+    val.style.color="black";
+});
+
+btn2.addEventListener("click",()=>{
+    val.innerText--;
+    if(val.innerText<0){
+        val.style.color = "red";
+    }else if(val.innerText == 0){
+        val.style.color = "black";
+    }
+});
